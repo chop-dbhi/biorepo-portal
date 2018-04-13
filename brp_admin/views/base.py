5d36dac79531c801a680ee4bc979f027699ddd8b
SueG <geraces@email.chop.edu>
2018-04-13 13:02:32 -0400

    Trigger Jenkins job to recache subjects (#149)
    * adjusting for rebase of dev
    
    * adjusting for rebase of dev branch
    
    * adjusting for rebase of dev
    
    adding elements to ehb test docker compose file
    
    removing dependency for ehb-service in docker compose file
    
    adding env file to dev docker compose file
    
    changing ehb to latest for dev docker compose
    
    removing admin-bootstrap from settings
    
    removing admin-bootstrapped from settings
    
    changing max log-in attemps from 10 to 5
    
    adjusting tests to accomidate change to number of login attempts.
    
    refactoring 'add user to protocol' function fixing issue #141
    
    adding cache ehb_label function to commands
    
    filtering data going to the console. fixing issue #143
    
    including updates for ehb-datasouces with changes required for nautilus upgrade
    
    updating dockerfile with upgraded ehb-datasources with changes for nautilus upgrade
    
    changing loging page for better user experience. see issue #146
    
    adding caching ehb lables function to the brp admin
    
    changing message on cache ehb labels page in the brp admin
    
    updating verbiage for forgot password page
    
    adjusting urls to add cache_labels to brp admin
    
    refactoring 'cache subjects' function in the admin page to trigger jenkins job 'brp_caching_nightly'
    
    adding requests library to dockerfile
    
    adding logging and exceptions to brp-caching function
    
    adding comments
    
    * removing duplicate line in brp/middleware.py

--- brp_admin/views/base.py
+++ brp_admin/views/base.py
@@ -1,4 +1,46 @@
-# import logging
-#
-# from django.shortcuts import get_object_or_404
-# from django.views.generic.base import TemplateView
+import requests
+import os
+import logging
+
+log = logging.getLogger(__name__)
+
+
+class JenkinsJob(object):
+    #########################################################################
+    # This object is used to interact with Jenkins through the jenkins API.
+    # this object needs a url and parameters(optional)
+    # url: must start with '@' character and be the address of the job. This can
+    #      be found in the jenkins configuration under 'build triggers' where
+    #      'trigger builds remotely' is checked.
+    # Parameters: is optional, only use if required for job. should be a dictionary
+    # format.
+    #########################################################################
+    def __init__(self, url, parameters={}):
+
+        self.job_url = url
+        self.querystring = parameters
+
+        self.headers = {
+            'Cache-Control': "no-cache",
+        }
+
+    def __get_credentials(self):
+
+        name = os.environ.get('jenkins_id')
+        token = os.environ.get('jenkins_token')
+
+        # if jenkins id or jenkins token is not found, generate an exception.
+        if (name is None or token is None):
+            log.debug("Jenkins Credentials do not exist.")
+            raise Exception("Jenkins_Credentials_DNE")
+        else:
+            return name + ":" + token
+
+    def trigger_jenkins_job(self):
+        try:
+            credentials = self.__get_credentials()
+            self.url = "https://" + credentials + self.job_url
+            response = requests.request("POST", self.url, headers=self.headers, params=self.querystring)
+            return response
+        except:
+            return {'error': 'Jenkins Credentials do not exist'}
