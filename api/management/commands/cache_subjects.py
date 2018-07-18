053afba9e603bcb01938d51c634e5748eb1398a0
Aaron Browne <aaron0browne@gmail.com>
2018-07-18 16:21:54 -0400

    Add comments for understanding to the API module (#79)
    * Add comments for understanding to the API module
    
    Signed-off-by: Aaron Browne <aaron0browne@gmail.com>
    
    * Working session with Aaron Browne to address some of the TODOs
    
    * Small indent fix and gitignore additions
    
    Signed-off-by: Aaron Browne <aaron0browne@gmail.com>
    
    * fixing indent formatting in api-models-protocols.py

--- api/management/commands/cache_subjects.py
+++ api/management/commands/cache_subjects.py
@@ -16,48 +16,95 @@ from rest_framework.response import Response
 
 
 class Command(BaseCommand):
+    """Cache subjects from given protocols locally.
+
+
+    """
+
+    help = 'Cache subjects from given protocols locally.'
 
     def add_arguments(self, parser):
+        """Set up the protocol_id argument."""
         parser.add_argument('protocol_id', nargs='+', type=str)
 
     def getExternalRecords(self, pds, subject, lbls):
+        """Get external records for a subject/protocol combination.
+
+        Returns external records for a given subject from a given protocol
+        after labelling them with EHB labels and the protocol id.
+        """
+
+        # Get the appropriate request handler for external records.
         er_rh = ServiceClient.get_rh_for(record_type=ServiceClient.EXTERNAL_RECORD)
         try:
+
+            # Retrieve the external records for the subject/protocol.
+            # Path argument is needed for ehb_datasources to make request.
             pds_records = er_rh.get(
                 external_system_url=pds.data_source.url, path=pds.path, subject_id=subject['id'])
+
+            # TODO: why is this necessary?
             time.sleep(0.05)
         except PageNotFound:
+
+            # Return an empty array if the subject/protocol is not found.
             pds_records = []
 
+        # Result array.
         r = []
         for ex_rec in pds_records:
-            # Convert ehb-client object to JSON and then parse as py dict
+
+            # Convert ehb-client object to JSON and then parse as py dict.
+            # TODO: the external record object should have this capability.
+            # We should examine this to refactor for simplicity. It is possible
+            # to remove json.loads for each json_from_identity call.
             e = json.loads(ex_rec.json_from_identity(ex_rec))
-            # Map label descriptions from the eHB to External Records
+
+            # Map label descriptions from the eHB to External Records.
             for label in lbls:
                 if e['label'] == label['id']:
                     if label['label'] == '':
                         e['label_desc'] = 'Record'
                     else:
                         e['label_desc'] = label['label']
+
+            # Add the protocol datasource id to the external record.
             e['pds'] = pds.id
             r.append(e)
 
         return r
 
     def cache_records(self, protocol_id):
+        """Cache subject records from a given protocol locally."""
+
+        # TODO: consider passing in a list if we ever need to cache a small
+        # number of protocols at one time. look at why only using first item in
+        # list.
         protocol_id = protocol_id[0]
         if protocol_id == 'all':
+            # Special "all" protocol gets all protocols.
             protocols = Protocol.objects.all()
         else:
             protocols = Protocol.objects.filter(id=int(protocol_id)).all()
+
+        # Get external record label request handler.
         er_label_rh = ServiceClient.get_rh_for(record_type=ServiceClient.EXTERNAL_RECORD_LABEL)
+
+        # Retrieve the actual external record labels.
         lbls = er_label_rh.query()
+
+        # Tell user how many protocols are being cached.
         print('Caching {0} protocol(s)...'.format(len(protocols)))
         for protocol in protocols:
+
+            # Tell user which protocol is being cached.
             print('Caching {}'.format(protocol))
+
+            # Get list of subjects and organizations in the protocol.
             subjects = protocol.getSubjects()
             organizations = protocol.organizations.all()
+
+            # Serialize retrieved subjects or continue if there are none.
             if subjects:
                 subs = [eHBSubjectSerializer(sub).data for sub in subjects]
             else:
@@ -67,7 +114,10 @@ class Command(BaseCommand):
             # append the name here for display downstream.
             for o in organizations:
                 ehb_orgs.append(o.getEhbServiceInstance())
-            # Check if the protocol has external IDs configured. If so retrieve them
+
+            # TODO: Explain this block, down to the `for sub in subs` loop.
+            # Check if the protocol has external IDs configured.
+            # If so, retrieve them.
             manageExternalIDs = False
 
             protocoldatasources = protocol.getProtocolDataSources()
@@ -89,11 +139,16 @@ class Command(BaseCommand):
                     raise
                     pass
 
+            # Transform subjects for ease of use.
             for sub in subs:
+
+                # Initialize new fields.
                 sub['external_records'] = []
                 sub['external_ids'] = []
                 sub['organization'] = sub['organization_id']
                 sub.pop('organization_id')
+
+                # Add external records from all data sources.
                 for pds in protocoldatasources:
                     try:
                         sub['external_records'].extend(self.getExternalRecords(pds, sub, lbls))
@@ -106,8 +161,10 @@ class Command(BaseCommand):
                         pass
 
 
+                # TODO: Explain this block.
                 if manageExternalIDs:
-                    # Break out external ids into a separate object for ease of use
+                    # Break out external ids into a separate object for ease of
+                    # use.
                     for record in sub['external_records']:
                         if record['external_system'] == 3:
                             try:
@@ -115,13 +172,19 @@ class Command(BaseCommand):
                             except:
                                 print("an error occured getting external records")
                                 print(sub['external_ids'])
+
+                # Add organization name to subject record for display, since
+                # organization IDs can vary across apps. (?)
                 for ehb_org in ehb_orgs:
                     if sub['organization'] == ehb_org.id:
                         sub['organization_name'] = ehb_org.name
+
+            # Cache the array of subjects.
             cache_key = 'protocol{0}_sub_data'.format(protocol.id)
             cache.set(cache_key, json.dumps(subs))
             cache.persist(cache_key)
 
     def handle(self, *args, **options):
+        """Handle command invocation."""
         self.cache_records(options['protocol_id'])
         print("caching complete")
--- api/models/protocols.py
+++ api/models/protocols.py
@@ -29,6 +29,8 @@ __all__ = ('DataSource',)
 
 
 class Organization(BaseWithImmutableKey):
+    """An Organization with a unique subject id label and EHB interaction."""
+
     # Name of the organization, e.g. CHOP
     name = models.CharField(max_length=255, unique=True)
     # Name of the unique subject identifier used by the organization, e.g. MRN
@@ -42,10 +44,12 @@ class Organization(BaseWithImmutableKey):
         ordering = ['name']
 
     def createEhbInstance(self):
-        '''
-        On save we need to make sure this organization exists in the
-        ehb-service, otherwise create it
-        '''
+        """Idempotently create this organization in the EHB.
+
+        Returns False if the record can't be found or created. Otherwise
+        returns True.
+        """
+
         rh = ServiceClient.get_rh_for(record_type=ServiceClient.ORGANIZATION)
         try:
             org = rh.get(name=self.name)
@@ -61,18 +65,25 @@ class Organization(BaseWithImmutableKey):
                 return False
 
     def save(self, *args, **kwargs):
+        """Save the organization to the db and the EHB.
 
-            if self.createEhbInstance():
-                super(Organization, self).save(*args, **kwargs)
-            else:
-                raise Exception(
-                    'Unable to create Organization record in ehb-service')
+        On save we need to make sure this organization exists in the
+        ehb-service, otherwise create it.
+        """
+
+        if self.createEhbInstance():
+            super(Organization, self).save(*args, **kwargs)
+        else:
+            raise Exception(
+                'Unable to create Organization record in ehb-service')
 
     def getEhbServiceInstance(self):
-        '''
+        """Get the Organization record from the EHB.
+
         Gets the record for this organization as maintained in the ehb service
-        PageNotFound Exception will be thrown if no record is found
-        '''
+        PageNotFound Exception will be thrown if no record is found.
+        """
+
         rh = ServiceClient.get_rh_for(record_type=ServiceClient.ORGANIZATION)
         return rh.get(name=self.name)
 
@@ -81,13 +92,14 @@ class Organization(BaseWithImmutableKey):
 
 
 class DataSource(Base):
-    '''
+    """An external data source with EHB interactions.
+
     This class represents an external data source used within the Biorepository
     Portal. It is the link between the brp app and the ehb-service. The field
     ehb_service_es_id is the id of the ExternalSystem record stored in the
     ehb-service corresponding to this DataSource. The name field should match
-    the name field in the ExternalSystem record
-    '''
+    the name field in the ExternalSystem record.
+    """
 
     name = models.CharField(
         max_length=200, unique=True, verbose_name='Data Source Name')
@@ -101,6 +113,13 @@ class DataSource(Base):
         verbose_name='EHB Service External System ID')
 
     def createEhbInstance(self):
+        """Idempotently create the Data Source in the EHB.
+
+        Returns False if the record is not found and the creation attempt
+        fails. Otherwise, the `ehb_servies_es_id` field is set and returns
+        True.
+        """
+
         rh = ServiceClient.get_rh_for(
             record_type=ServiceClient.EXTERNAL_SYSTEM)
         try:
@@ -121,10 +140,12 @@ class DataSource(Base):
                 return False
 
     def save(self, *args, **kwargs):
-        '''
+        """Save the Data Source to the db and the EHB.
+
         On save we need to make sure this source exists in the ehb service,
         otherwise create it
-        '''
+        """
+
         if self.createEhbInstance():
             super(DataSource, self).save(*args, **kwargs)
         else:
@@ -132,8 +153,18 @@ class DataSource(Base):
                 'Unable to create ExternalSystem record in ehb-service')
 
     def clean(self):
+        """Prepare the DataSource for save.
+
+        Raises ValidationError if an External System exists in the EHB with
+        either the same name or same URL as this record. Changes to match
+        the existing name if one with the same URL exists.
+        """
+
         rh = ServiceClient.get_rh_for(
             record_type=ServiceClient.EXTERNAL_SYSTEM)
+
+        # Ensure an EHB data source does not already exist with the same URL
+        # but a different name.
         try:
             es = rh.get(url=self.url)
             if(self.name != es.name):
@@ -149,6 +180,8 @@ class DataSource(Base):
             # The save method will create the record in the ehb-service
             pass
 
+        # Ensure an EHB data source does not already exist with the same name
+        # but a different URL.
         try:
             es = rh.get(name=self.name)
             if(self.url != es.url):
@@ -165,9 +198,11 @@ class DataSource(Base):
         return self.name
 
     def getExternalSystem(self):
-        '''
-        This returns the ehb-service representation of this DataSource
-        '''
+        """Return the ehb-service representation of this DataSource.
+
+        Set the `ehb_service_es_id` field to match the EHB instance.
+        """
+
         rh = ServiceClient.get_rh_for(
             record_type=ServiceClient.EXTERNAL_SYSTEM)
         es = rh.get(url=self.url)
@@ -179,10 +214,12 @@ class DataSource(Base):
         return es
 
     def getSubjects(self):
-        '''
+        """Return subjects that have records in this data source.
+
         Returns the ehb-service representation of the subjects that have
         external_records for this data_source
-        '''
+        """
+
         es = self.getExternalSystem()
         if es:
             rh = ServiceClient.get_rh_for(
@@ -191,9 +228,14 @@ class DataSource(Base):
 
 
 class Protocol(BaseWithImmutableKey):
-    '''
-    This class holds the information necessary for Protocol
-    '''
+    """A Protocol class with DataSources, Users, and Orgs.
+
+    The DataSources, Users, and Orgs are all linked through ManyToManyFields.
+    EHB interactions to ensure the Protocol is saved as a group in the EHB as
+    well as return subjects, add a subject, and test for subject membership.
+    Methods for user authorization and access to ProtocolDataSource linkage
+    records are also included.
+    """
 
     class Meta(BaseWithImmutableKey.Meta):
         ordering = ['name']
@@ -209,12 +251,19 @@ class Protocol(BaseWithImmutableKey):
         return self.name
 
     def ehb_group_name(self):
+        """The group name used to store and access the EHB record."""
         return 'BRP:' + self.immutable_key.key
 
     def _client_key(self):
+        """The EHB authorization key."""
         return self._settings_prop('CLIENT_KEY', 'key', 'xyz123abc987')
 
     def createEhbProtocolGroup(self):
+        """Idempotently save the protocol as an EHB group.
+
+        Raises an Exception if the group cannot be created.
+        """
+
         gh = ServiceClient.get_rh_for(record_type=ServiceClient.GROUP)
         try:
             gh.get(name=self.ehb_group_name())
@@ -231,6 +280,11 @@ class Protocol(BaseWithImmutableKey):
                     'Unable to create Protocol Subject Group in ehb-service')
 
     def save(self, *args, **kwargs):
+        """Save the protocol in the db and the EHB."""
+
+        # Call the super save method first to create an immutable key, which is
+        # needed to create the group name that is used to save a record in the
+        # EHB.
         super(Protocol, self).save(*args, **kwargs)
         # Need to create a group for this protocol if it doesn't exist
         # (i.e. before there is a pk) that will be used to identify subjects
@@ -238,9 +292,15 @@ class Protocol(BaseWithImmutableKey):
         self.createEhbProtocolGroup()
 
     def _gh(self):
+        """Get the request handler for the EHB group."""
         return ServiceClient.get_rh_for(record_type=ServiceClient.GROUP)
 
     def _subject_group(self):
+        """Get the EHB group with authorization key.
+
+        Returns None if an Exception is raised during operation.
+        """
+
         try:
             grp = self._gh().get(name=self.ehb_group_name())
             grp.client_key = self._client_key()
@@ -249,6 +309,11 @@ class Protocol(BaseWithImmutableKey):
             return None
 
     def addSubject(self, subject):
+        """Add a subject to the protocol group in the EHB.
+
+        Returns False if an Exception is raised during operation.
+        """
+
         try:
             r = self._gh().add_subjects(self._subject_group(), [subject])
             return r[0].get('success')
@@ -256,26 +321,39 @@ class Protocol(BaseWithImmutableKey):
             return False
 
     def getSubjects(self):
-        '''
-        Returns the Subjects on this protocol
-        '''
+        """Returns the Subjects on this protocol.
+
+        Gets all subjects related to this protocol from the EHB.
+        """
+
         try:
             return self._gh().get_subjects(self._subject_group())
         except Exception:
             return None
 
     def isSubjectOnProtocol(self, subject):
+        """Tests if a subject is related to this protocol."""
         return subject in self.getSubjects()
 
     def getProtocolDataSources(self):
+        """Returns all the Protocol to DataSource linkage records."""
         return ProtocolDataSource.objects.filter(protocol=self)
 
     def isUserAuthorized(self, user):
+        """Tests if a user is authorized for this protocol."""
         return user in self.users.all()
 
 
 class ProtocolDataSource(Base):
+    """A Protocal to DataSource linkage with EHB interactions and driver access.
 
+    Includes the DataSource driver backend and configuration as well as the
+    path associated with the Protocol in the DataSource. Methods are provided
+    to test if an external record is part of the Protocol, to get a subject
+    from the EHB, to get external records for a subject from the EHB, and to
+    get external identifiers for a subject from the EHB. The configured driver
+    backend instance can also be accessed.
+    """
     class Meta(Base.Meta):
         ordering = ['protocol']
 
@@ -309,9 +387,15 @@ class ProtocolDataSource(Base):
     )
 
     def _isSecure(self):
+        """Test if the related DataSource is secure (uses https)."""
         return self.data_source.url.startswith('https')
 
     def clean(self):
+        """Ensure the driver configuration is valid before saving.
+
+        Raises ValidationError if the configuration is not valid JSON.
+        """
+
         try:
             json.loads(self.driver_configuration)
         except ValueError:
@@ -321,6 +405,12 @@ class ProtocolDataSource(Base):
             raise ValidationError(msg)
 
     def isRecordInPDS(self, record, subject):
+        """Tests if an external record is part of this protocol.
+
+        The method requires the subject that the record refers to, in order to
+        run the EHB query.
+        """
+
         er_rh = ServiceClient.get_rh_for(record_type=ServiceClient.EXTERNAL_RECORD)
         # TODO cache candidate
         pds_records = er_rh.get(
@@ -332,6 +422,12 @@ class ProtocolDataSource(Base):
 
 
     def getSubject(self, subjectId):
+        """Get a subject from the EHB by subject id.
+
+        Raises Http404 if the subject is not found or is not part of the
+        related protocol.
+        """
+
         cache_key = '{}_subjects'.format(self.protocol.id)
         cached = cache.get(cache_key)
         if cached:
@@ -343,6 +439,8 @@ class ProtocolDataSource(Base):
             try:
                 s_rh = ServiceClient.get_rh_for(record_type=ServiceClient.SUBJECT)
                 subject = s_rh.get(id=subjectId)
+
+                # If the subject is not in this protocol, raise an error.
                 if not self.protocol.isSubjectOnProtocol(subject):
                     raise Http404
                 else:
@@ -350,12 +448,12 @@ class ProtocolDataSource(Base):
             except PageNotFound:
                 raise Http404
 
+        # TODO: Is this statement ever reached? If not, refactor for clarity.
         return None
 
     def getSubjectExternalRecords(self, subject):
-        '''
-        Return all external records for a given subject on this Protocol
-        '''
+        """Return all external records for a given subject on this Protocol."""
+
         er_rh = ServiceClient.get_rh_for(record_type=ServiceClient.EXTERNAL_RECORD)
         erl_rh = ServiceClient.get_rh_for(record_type=ServiceClient.EXTERNAL_RECORD_LABEL)
         labels = cache.get('ehb_labels')
@@ -373,6 +471,7 @@ class ProtocolDataSource(Base):
         return RecordUtils.serialize_external_records(self, pds_records, labels)
 
     def getExternalIdentifiers(self, subject, labels):
+        # TODO: Understand what external identifiers are...
         er_rh = ServiceClient.get_rh_for(record_type=ServiceClient.EXTERNAL_RECORD)
         ck = '{0}_{1}_externalrecords'.format(self.protocol.id, subject.id)
         # See if our records are in the cache.
@@ -399,6 +498,8 @@ class ProtocolDataSource(Base):
         return pds_records
 
     def getDriver(self, protocol_user_credentials):
+        """Get a configured driver backend instance for the DataSource."""
+
         driver = None
         if self.driver == ProtocolDataSourceConstants.redcap_driver:
             pw = protocol_user_credentials.data_source_password
@@ -442,13 +543,13 @@ class ProtocolDataSource(Base):
 
 
 class ProtocolDataSourceLink(models.Model):
-    '''
-    This model is used to link records in two protocol data sources.
+    """This model is used to link records in two protocol data sources.
 
     The actions required to link the data sources are performed by datasource
     plugins. The available plugins are set in the PLUGINS variable in the
     django settings
-    '''
+    """
+
     pds_one = models.ForeignKey(
         ProtocolDataSource,
         verbose_name="Protocol Data Source",
@@ -486,12 +587,14 @@ class ProtocolDataSourceLink(models.Model):
         return self.linker.split(',')[1]
 
     def clean(self):
-        '''
+        """Ensure no object with the same PDSs and Linker exists already.
+
         The normal unique_together constraint doesn't work because we want to
         ensure that for two protocol data sources, say A and B, and a linker,
         say L, that only one ProtocolDataSourceLink record can be created
-        regardless of what position A and B are held in pds_one and pds_two
-        '''
+        regardless of what position A and B are held in pds_one and pds_two.
+        """
+
         try:
             msg = (
                 'This protocol data source linking option has already been' +
@@ -501,19 +604,22 @@ class ProtocolDataSourceLink(models.Model):
                 pds_two=self.pds_two).filter(
                 linker=self.linker)
             for entry in order_one:
+
+                # If they are equal we are modifying this record
                 if entry.pk != self.pk:
-                    # If they are equal we are modifying this record
                     raise ValidationError(msg)
             order_two = ProtocolDataSourceLink.objects.filter(
                 pds_one=self.pds_two).filter(
                 pds_two=self.pds_one).filter(
                 linker=self.linker)
             for entry in order_two:
+
+                # If they are equal we are modifying this record
                 if entry.pk != self.pk:
-                    # If they are equal we are modifying this record
                     raise ValidationError(msg)
+
+        # The ProtocolDataSource validation will handle this
         except ProtocolDataSource.DoesNotExist:
-            # The ProtocolDataSource validation will handle this
             pass
 
     class Meta(object):
@@ -521,11 +627,12 @@ class ProtocolDataSourceLink(models.Model):
 
 
 class ProtocolUser(Base):
-    '''
-    This class is join between a Protocol and an Portal User.
+    """
+    This class joins a Protocol and an Portal User.
 
-    It defines that User's role (permission level for the protocol)
-    '''
+    It defines that User's role (permission level for the protocol).
+    """
+
     protocol = models.ForeignKey(Protocol)
     user = models.ForeignKey(User)
     roles = (
@@ -542,10 +649,14 @@ class ProtocolUser(Base):
 
 
 class ProtocolUserCredentials(Base):
-    '''
+    """Credentials for a Portal User to access a DataSource.
+
     This class holds information necessary for Portal user to access a given
-    DataSource for a given protocol
-    '''
+    DataSource for a given protocol.
+    """
+
+    # TODO: Some of the below fields seem like unecessary duplicates...
+
     protocol = models.ForeignKey(Protocol, verbose_name='Protocol')
     data_source = models.ForeignKey(
         ProtocolDataSource, verbose_name='Protocol Data Source')
