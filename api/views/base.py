7414320f93e6ec7cf9ce759302a38bd5434154da
seg1129 <geraces@email.chop.edu>
2018-09-27 12:39:47 -0400

    adding pedigree relationship ehb client request handeler to BRP api view

--- api/views/base.py
+++ api/views/base.py
@@ -8,7 +8,7 @@ from api.utilities import SubjectUtils, RecordUtils
 class BRPApiView(APIView):
     # Cache
     cache = _cache
-    # Honest Broker Handlers
+    # Honest Broker Request Handelers
     s_rh = ServiceClient.get_rh_for(record_type=ServiceClient.SUBJECT)
     o_rh = ServiceClient.get_rh_for(record_type=ServiceClient.ORGANIZATION)
     es_rh = ServiceClient.get_rh_for(record_type=ServiceClient.EXTERNAL_SYSTEM)
@@ -16,6 +16,7 @@ class BRPApiView(APIView):
     erl_rh = ServiceClient.get_rh_for(record_type=ServiceClient.EXTERNAL_RECORD_LABEL)  # noqa
     err_rh = ServiceClient.get_rh_for(record_type=ServiceClient.EXTERNAL_RECORD_RELATION)  # noqa
     g_rh = ServiceClient.get_rh_for(record_type=ServiceClient.GROUP)
+    relationship_HB_handler = ServiceClient.get_rh_for(record_type=ServiceClient.PEDIGREERELATIONSHIP)
     # Utility Classes
     subject_utils = SubjectUtils
     record_utils = RecordUtils
