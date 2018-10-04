bc53a9e6fbed37742cfa76a34521d77386de4d5a
seg1129 <geraces@email.chop.edu>
2018-10-04 10:54:35 -0400

    setting up relationship API py call file for #196

---
+++ api/views/relationship.py
@@ -0,0 +1,10 @@
+from ehb_client.requests.pedigree_relationship_handler import PedigreeRelationship
+from .base import BRPApiView
+
+
+class RelationshipDetailView(BRPApiView):
+    def post(self):
+        pass
+
+    def get(self):
+        pass
