671922499b03c7bbb303fbca015db3df548757cb
seg1129 <geraces@email.chop.edu>
2018-12-21 11:40:46 -0500

    initial setup of getting relationships through API and rendering on subject view page

--- react_ui/components/SubjectView/PedigreePanel/PedigreeCardView.js
+++ react_ui/components/SubjectView/PedigreePanel/PedigreeCardView.js
@@ -1,10 +1,34 @@
 import React from 'react';
 import { Link } from 'react-router';
 import { connect } from 'react-redux';
+import * as PedigreeActions from '../../../actions/pedigree';
 
-// const SubjectPedigreeCardView = (props) => {
+
 class PedigreeCardView extends React.Component {
-  // const subject = props.subject.activeSubject;
+
+  constructor(props) {
+    super(props);
+  }
+  componentWillUnmount() {
+    const { dispatch } = this.props;
+    dispatch(PedigreeActions.fetchPedigree(
+      this.props.protocol.activeProtocolId,
+      this.props.subject.id));
+  }
+  renderRelationships(relationships){
+    return (
+      recordNodes ?
+        <table className="table table-striped">
+          <thead>
+            <tr><th>Relation</th><th>Record</th><th>Created</th><th>Modified</th></tr>
+          </thead>
+          <tbody>
+            {relationships}
+          </tbody>
+        </table> : <div>No Relationships</div>
+    );
+  }
+
   render() {
     return (
       <div className="col-md-4 col-sm-6">
