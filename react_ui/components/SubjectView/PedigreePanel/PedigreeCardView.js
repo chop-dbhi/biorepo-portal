f19765a9cd02cfb666effcee12593fb77a979e7d
seg1129 <geraces@email.chop.edu>
2019-01-14 21:18:50 -0500

    WIP: making API call to get pedigree from react UI

--- react_ui/components/SubjectView/PedigreePanel/PedigreeCardView.js
+++ react_ui/components/SubjectView/PedigreePanel/PedigreeCardView.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {PropTypes} from 'react';
+// import PropTypes from 'prop-types'
 import { Link } from 'react-router';
 import { connect } from 'react-redux';
 import * as PedigreeActions from '../../../actions/pedigree';
@@ -8,77 +9,167 @@ class PedigreeCardView extends React.Component {
 
   constructor(props) {
     super(props);
+    this.state = { relationships: [],};
+    console.log('we are in constructor')
   }
-  componentWillUnmount() {
+  componentDidMount() {
+    console.log('we are in componentDidMount')
     const { dispatch } = this.props;
     dispatch(PedigreeActions.fetchPedigree(
-      this.props.protocol.activeProtocolId,
+      this.props.activeProtocolId,
       this.props.subject.id));
-  }
-  renderRelationships(relationships){
-    return (
-      relationships ?
-        <table className="table table-striped">
-          <thead>
-            <tr><th>Relation</th><th>Organization ID</th></tr>
-          </thead>
-          <tbody>
-            {relationships}
-          </tbody>
-        </table> : <div>No Relationships</div>
-    );
-  }
+      // .then(results => {
+      //     console.log(results)
+      //     // return results.json();
+      //     return results;
+      //   }).then(data => {
+      //     // let this.state.relationships = data.results
+      //     console.log('data')
+      //     console.log(data)
+      //     this.setState({relationships: data})
+
+    // console.log(this.props.activeProtocolId)
+    // console.log('Subject ID')
+    // console.log(this.props.subject.id)
+    // console.log('fetch in Component did mount')
+    // console.log(fetch(PedigreeActions.fetchPedigree(
+    //   this.props.activeProtocolId,
+    //   this.props.subject.id)))
+    // dispatch(PedigreeActions.fetchPedigree(
+    //   this.props.activeProtocolId,
+    //   this.props.subject.id))
+    //   .then(results => {
+    //     console.log(results)
+    //     // return results.json();
+    //     return results;
+    //   }).then(data => {
+    //     // let this.state.relationships = data.results
+    //     console.log('data')
+    //     console.log(data)
+    //     this.setState({relationships: data})
+    //   })
+
+      // this.setState({relationships: relationships});
+
+  // })
+}
+
+  // renderRelationships(relationships){
+  //   return (
+  //     relationships ?
+  //       <table className="table table-striped">
+  //         <thead>
+  //           <tr><th>Relation</th><th>Organization ID</th></tr>
+  //         </thead>
+  //         <tbody>
+  //           {this.state.relationships}
+  //         </tbody>
+  //       </table> : <tr>No Relationships</tr>
+  //   );
+  // }
 
   render() {
-    relationships = PedigreeActions.fetchPedigree(
-                            this.props.protocol.activeProtocolId,
-                            this.props.subject.id);
-    if (relationships.length !== 0) {
-      relationships = relationships.map((relationships, i) => {
-        let linkIcon = null;
+    console.log('we are in render')
+    const protocol = this.props.activeProtocolId;
+    // const { relationships } = this.state;
+    console.log('this.state')
+    console.log(this.state)
+
+    // relationships = PedigreeActions.fetchPedigree(
+    //                         protocol,
+    //                         this.props.subject.id);
+    console.log('this.props.pedigree')
+    console.log(this.props.pedigree)
+    const relationships = this.state.relationships;
+    console.log('relationships')
+    console.log(relationships)
+    if (this.props.pedigree != []) {
+      this.state.relationships.map((relationship, i) => {
+        console.log('relationship subject_1')
+        console.log(this.state.relationship.subject_1)
+        // let linkIcon = null;
         // TODO: Factor out these record lines into their own components.
-        if (this.props.activePedigree != null && (this.props.activePedigree.id === relationship.id)) {
-          return (
-            // TODO: add onclick function to add ability to edit/add a pedigree relationship
-            <tr>
-              <td>{relationship.id}</td>
-              <td>{relationship.subject_1_role}</td>
-            </tr>
-          );
-        }
+        // if (this.props.activePedigree != null && (this.props.activePedigree.id === relationship.id)) {
+        //   return (
+        //     // TODO: add onclick function to add ability to edit/add a pedigree relationship
+        //     <tr>
+        //       <td>{relationship.id}</td>
+        //       <td>{relationship.subject_1_role}</td>
+        //     </tr>
+        //   );
+        // }
+// }
 
-
-    return (
-      <div className="col-md-4 col-sm-6">
-        <div className="card">
-          <div className="more">
-              <i className="ti-pencil"></i>
+      return (
+        <div className="col-md-4 col-sm-6">
+          <div className="card">
+            <div className="more">
+                <i className="ti-pencil"></i>
+            </div>
+            <table className="table table-striped">
+              <thead>
+                <tr><th>Relation</th><th>Organization ID</th></tr>
+              </thead>
+              <tbody>
+                {this.state.relationship.subject_1}
+              </tbody>
+            </table>
           </div>
-          <div className="content">
-            <h4 className="title">Relationships</h4>
-            <p className="description">Mother</p>
-            <p className="description">Father</p>
-          </div>
-          <tr>
-            <td>{relationship.id}</td>
-            <td>{linkIcon} {relationship.subject_1_role}</td>
-          </tr>
         </div>
+      );
+  });
+} else { return (
+  <div className="col-md-4 col-sm-6">
+    <div className="card">
+      <div className="more">
+        <i className="ti-pencil"></i>
       </div>
-    );
-  }, this);
+      <div className="content">
+        <h4 className="title">Relationships</h4>
+        <p className="description">Mother</p>
+        <p className="description">Father</p>
+      </div>
+    </div>
+  </div>
+);
+    // return (
+    //   <div className="col-md-4 col-sm-6">
+    //     <div className="card">
+    //       <div className="more">
+    //           <i className="ti-pencil"></i>
+    //       </div>
+    //       <div className="content">
+    //         <h4 className="title">Relationships</h4>
+    //         <p className="description">Mother</p>
+    //         <p className="description">Father</p>
+    //       </div>
+    //     </div>
+    //   </div>
+    // )
   }
   }
 }
 
+PedigreeCardView.propTypes = {
+  protocol: React.PropTypes.object,
+  subject: React.PropTypes.object,
+  activeRecord: React.PropTypes.object,
+  activeProtocolId: React.PropTypes.number,
+  pedigree: React.PropTypes.object,
+  // isFetching: React.PropTypes.bool,
+};
+
 function mapStateToProps(state) {
   return {
-    relationships: {
-      isFetching: state.record.isFetching,
-    },
+    // pedigree: state.pedigree.activePedigree,
+    //   // isFetching: state.relationships.isFetching,
+    //   pedigree: state.relationships.pedigree,
+    // },
     subject: state.subject.activeSubject,
     activeRecord: state.record.activeRecord,
-    };
-  }
-// export default connect(SubjectPedigreeCardView);
+    activeProtocol: state.protocol.activeProtocol,
+    activeProtocolId: state.protocol.activeProtocolId,
+  };
+}
+// export default connect(mapStateToProps);
 export default connect(mapStateToProps)(PedigreeCardView);
