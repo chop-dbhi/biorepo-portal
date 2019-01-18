bc787578878d3f6d96d0e95fc09ee2c2604d7d11
seg1129 <geraces@email.chop.edu>
2019-01-18 10:40:56 -0500

    cleaning up actions/pedigree and PedigreeCardView

--- react_ui/actions/pedigree.js
+++ react_ui/actions/pedigree.js
@@ -1,6 +1,18 @@
+
+import fetch from 'isomorphic-fetch';
+
 export const REQUEST_PEDIGREE = 'REQUEST_PEDIGREE';
 export const RECEIVE_PEDIGREE = 'RECEIVE_PEDIGREE';
-export const SET_ACTIVE_PEDIGREE = 'SET_ACTIVE_PEDIGREE'
+export const SET_ACTIVE_PEDIGREE = 'SET_ACTIVE_PEDIGREE';
+
+function checkResponse(response) {
+  if (response.status >= 200 && response.status < 300) {
+    return response;
+  }
+  const error = new Error(response.statusText);
+  error.response = response;
+  throw error;
+}
 
 export function requestPedigree() {
   return {
@@ -9,14 +21,14 @@ export function requestPedigree() {
 }
 
 export function receivePedigree(json) {
-  return {
-    type: REQUEST_PEDIGREE_SUCCESS,
-    pedigree: json,
-  };
-}
+    return{
+      type: RECEIVE_PEDIGREE,
+      pedigree: json,
+    };
+  }
 
 export function fetchPedigree(protocolID, subjectId) {
-  const url = `protocols/${protocolID}/pedigree/subject/${subjectId}/`;
+  const url = `api/protocols/${protocolID}/pedigree/subject/${subjectId}`;
   return dispatch => {
     dispatch(requestPedigree());
     return fetch(url, {
@@ -26,22 +38,24 @@ export function fetchPedigree(protocolID, subjectId) {
         Authorization: `token ${token}`,
       },
     })
-      // .then(checkResponse)
-      .then(response => response.json())
-      .then(json => dispatch(receivePedigree(json)))
-      .catch(error => {
-        dispatch(NotificationActions.addNotification(
-          {
-            message: 'Error Contacting the electronic Honest Broker',
-            level: 'error',
-            error,
-          }
-        ));
 
-        // This is a bit of a hack to get the Notification System to render properly.
-        dispatch(NotificationActions.renderNotification());
-      }
-    );
+  // };
+      .then(checkResponse)
+      .then(response => response.json())
+      .then(json => dispatch(receivePedigree(json)));
+    //   .catch(error => {
+    //     dispatch(NotificationActions.addNotification(
+    //       {
+    //         message: 'Error Contacting the electronic Honest Broker',
+    //         level: 'error',
+    //         error,
+    //       }
+    //     ));
+    //
+    //     // This is a bit of a hack to get the Notification System to render properly.
+    //     dispatch(NotificationActions.renderNotification());
+    //   }
+    // );
   };
 }
 
--- react_ui/components/SubjectView/PedigreePanel/PedigreeCardView.js
+++ react_ui/components/SubjectView/PedigreePanel/PedigreeCardView.js
@@ -1,5 +1,4 @@
 import React from 'react';
-// import PropTypes from 'prop-types'
 import { Link } from 'react-router';
 import { connect } from 'react-redux';
 import * as PedigreeActions from '../../../actions/pedigree';
@@ -19,80 +18,10 @@ class PedigreeCardView extends React.Component {
     dispatch(PedigreeActions.fetchPedigree(
       this.props.activeProtocolId,
       this.props.subject.id));
-    //   .then(results => {
-    //     console.log(results)
-    //     // return results.json();
-    //     return results;
-    //   }).then(data => {
-    //     // let this.state.relationships = data.results
-    //     console.log('data')
-    //     console.log(data)
-    //     this.setState({relationships: data})
-    //   })
-
-      // this.setState({relationships: relationships});
-
-  // })
 }
-
-  // renderRelationships(relationships){
-  //   return (
-  //     relationships ?
-  //       <table className="table table-striped">
-  //         <thead>
-  //           <tr><th>Relation</th><th>Organization ID</th></tr>
-  //         </thead>
-  //         <tbody>
-  //           {this.state.relationships}
-  //         </tbody>
-  //       </table> : <tr>No Relationships</tr>
-  //   );
-  // }
-
   render() {
-    console.log('we are in render')
-    console.log('this.props.pedigree.items')
-    console.log(this.props.pedigree.items.items)
     const protocol = this.props.activeProtocolId;
     const relationships = this.props.pedigree.items.items;
-    // if (this.props.pedigree.items == null){
-    //   const relationships = [];
-    // } else {
-    //   const relationships = this.props.pedigree.items;
-    // }
-    // const { relationships } = this.state;
-    console.log('this.state')
-    console.log(this.state)
-
-    // relationships = PedigreeActions.fetchPedigree(
-    //                         protocol,
-    //                         this.props.subject.id);
-    console.log('this.state.pedigree')
-    console.log(this.props.pedigree)
-    // const relationships = this.state.relationships;
-    console.log('relationships')
-    console.log( relationships )
-    // if (this.props.pedigree.items) {
-    //   this.props.pedigree.items.map((relationship, i) => {
-    //     console.log('relationship subject_1')
-    //     console.log(this.props.pedigree.items.subject_1)
-
-        // let linkIcon = null;
-        // TODO: Factor out these record lines into their own components.
-        // if (this.props.activePedigree != null && (this.props.activePedigree.id === relationship.id)) {
-        //   return (
-        //     // TODO: add onclick function to add ability to edit/add a pedigree relationship
-        //     <tr>
-        //       <td>{relationship.id}</td>
-        //       <td>{relationship.subject_1_role}</td>
-        //     </tr>
-        //   );
-        // }
-// }
-
-
-
-
       if (relationships.length != 0){
         return (
           <div className="col-md-4 col-sm-6">
@@ -132,38 +61,8 @@ class PedigreeCardView extends React.Component {
           </div>
         </div>
       )
-  // },this);
-
-//   <div className="col-md-4 col-sm-6">
-//     <div className="card">
-//       <div className="more">
-//         <i className="ti-pencil"></i>
-//       </div>
-//       <div className="content">
-//         <h4 className="title">Relationships</h4>
-//         <p className="description">Mother</p>
-//         <p className="description">Father</p>
-//       </div>
-//     </div>
-//   </div>
-// );
-    // return (
-    //   <div className="col-md-4 col-sm-6">
-    //     <div className="card">
-    //       <div className="more">
-    //           <i className="ti-pencil"></i>
-    //       </div>
-    //       <div className="content">
-    //         <h4 className="title">Relationships</h4>
-    //         <p className="description">Mother</p>
-    //         <p className="description">Father</p>
-    //       </div>
-    //     </div>
-    //   </div>
-    // )
+    }
   }
-  }
-
 
 PedigreeCardView.propTypes = {
   dispatch: React.PropTypes.func,
@@ -174,13 +73,7 @@ PedigreeCardView.propTypes = {
   pedigree: React.PropTypes.object,
   isFetching: React.PropTypes.bool,
 };
-// function mapDispatchToProps (dispatch) {
-//   return {
-//     pedigree: () => dispatch(PedigreeActions.fetchPedigree(
-//       this.props.activeProtocolId,
-//       this.props.subject.id))
-//   }
-// }
+
 function mapStateToProps(state) {
   return {
     pedigree: {
@@ -193,5 +86,5 @@ function mapStateToProps(state) {
     activeProtocolId: state.protocol.activeProtocolId,
   };
 }
-// export default connect(mapStateToProps);
+
 export default connect(mapStateToProps)(PedigreeCardView);
