dfcb88f1f756a2c856a11dca8c10347f3b110f4f
seg1129 <geraces@email.chop.edu>
2019-01-28 13:03:19 -0500

    adding states and functions to add a new pedigree relationship in the subject view

--- react_ui/actions/pedigree.js
+++ react_ui/actions/pedigree.js
@@ -4,6 +4,10 @@ import fetch from 'isomorphic-fetch';
 export const REQUEST_PEDIGREE = 'REQUEST_PEDIGREE';
 export const RECEIVE_PEDIGREE = 'RECEIVE_PEDIGREE';
 export const SET_ACTIVE_PEDIGREE = 'SET_ACTIVE_PEDIGREE';
+export const ADD_PEDIGREE_REL_SUCCESS = 'ADD_PEDIGREE_REL_SUCCESS';
+export const ADD_PEDIGREE_REL_REQUEST = 'ADD_PEDIGREE_REL_REQUEST';
+export const ADD_PEDIGREE_REL_FAILURE = 'ADD_PEDIGREE_REL_FAILURE';
+export const SET_ADD_PEDIGREE_REL_MODE = 'SET_ADD_PEDIGREE_REL_MODE';
 
 function checkResponse(response) {
   if (response.status >= 200 && response.status < 300) {
@@ -56,6 +60,78 @@ export function fetchPedigree(protocolID, subjectId) {
     //     dispatch(NotificationActions.renderNotification());
     //   }
     // );
+  };
+}
+
+export function checkAddPedigreeRel(json) {
+  const [success, relationship, errors] = json;
+  if (success) {
+    return relationship;
+  }
+  const error = new Error('Unable to add subject');
+  error.errors = errors;
+  throw error;
+}
+
+export function setAddPedigreeRelMode(mode = null) {
+  // Update state to enable or disable AddSubject mode
+  return {
+    type: SET_ADD_PEDIGREE_REL_MODE,
+    mode,
+  };
+}
+
+export function addPedigreeRelRequest() {
+  return {
+    type: ADD_PEDIGREE_REL_REQUEST,
+  };
+}
+
+export function addPedigreeRelSuccess(pedigreeRel) {
+  return dispatch => {
+    dispatch(NotificationActions.addNotification(
+      {
+        message: 'Relationship Added',
+        level: 'success',
+        autoDismiss: 2,
+      }
+    ));
+    dispatch(setAddPedigreeRelMode());
+    dispatch({
+      type: ADD_PEDIGREE_REL_SUCCESS,
+      isSaving: false,
+      pedigreeRel,
+    });
+  };
+}
+
+export function addPedigreeRelFailure(error) {
+  const errors = error.errors;
+  return {
+    type: ADD_PEDIGREE_REL_FAILURE,
+    errors,
+  };
+}
+
+// TODO: add pedigree relationship to the eHB
+export function addPedigreeRel(protocolId, pedigreeRel) {
+  return dispatch => {
+    dispatch(addPedigreeRelRequest());
+    const url = `api/protocols/${protocolId}/subjects/create`;
+    return fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+        Authorization: `token ${token}`,
+        'X-CSRFToken': csrf_token,
+      },
+      body: JSON.stringify(pedigreeRel),
+    })
+      .then(response => response.json())
+      .then(checkAddPedigreeRel)
+      .then(pedigreeRel => dispatch(addPedigreeRelSuccess(pedigreeRel)))
+      .catch(errors => dispatch(addPedigreeRelFailure(errors)));
   };
 }
 
