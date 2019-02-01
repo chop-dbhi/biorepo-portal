e59775e9b3a6f7641a2a301d4cebad4f8a5153e9
seg1129 <geraces@email.chop.edu>
2019-02-01 16:12:19 -0500

    adding addition reducers to handle act of adding relationships

--- react_ui/reducers/pedigree.js
+++ react_ui/reducers/pedigree.js
@@ -1,4 +1,7 @@
-import { REQUEST_PEDIGREE, RECEIVE_PEDIGREE, SET_ACTIVE_PEDIGREE, CLEAR_PEDIGREE_STATE } from '../actions/pedigree';
+import { REQUEST_PEDIGREE, RECEIVE_PEDIGREE, SET_ACTIVE_PEDIGREE,
+         CLEAR_PEDIGREE_STATE, SET_ADD_PEDIGREE_REL, SET_ADD_PEDIGREE_REL_MODE,
+         ADD_PEDIGREE_REL_SUCCESS, ADD_PEDIGREE_REL_REQUEST,
+         ADD_PEDIGREE_REL_FAILURE, SET_UPDATE_FORM_ERRORS} from '../actions/pedigree';
 
 const initialState = {
   isFetching: false,
@@ -19,10 +22,50 @@ function pedigree(state = initialState, action){
       });
     case SET_ACTIVE_PEDIGREE:
       return Object.assign({}, state, {
-        activeProtocolId: parseInt(action.pedigreeId, 10),
+        activePedigreeId: parseInt(action.pedigreeId, 10),
       });
     case CLEAR_PEDIGREE_STATE:
       return initialState;
+    case SET_ADD_PEDIGREE_REL:
+      return Object.assign({}, state, {
+        newPedigreeRel: action.pedigreeRel,
+      });
+    case SET_ADD_PEDIGREE_REL_MODE:
+      if (action.mode != null) {
+        return Object.assign({}, state, {
+          addPedigreeRelMode: action.mode,
+        });
+      }
+      return Object.assign({}, state, {
+        addPedigreeRelMode: !action.mode,
+      });
+    case ADD_PEDIGREE_REL_REQUEST:
+      return Object.assign({}, state, {
+        isSaving: true,
+      });
+    case ADD_PEDIGREE_REL_SUCCESS:
+      return Object.assign({}, state, {
+        newFormErrors: {
+          server: [],
+          form: [],
+        },
+        isSaving: false,
+      });
+    case ADD_PEDIGREE_REL_FAILURE:
+      return Object.assign({}, state, {
+        newFormErrors: {
+          server: action.errors,
+          form: [],
+        },
+        isSaving: false,
+      });
+    case SET_UPDATE_FORM_ERRORS:
+      return Object.assign({}, state, {
+        updateFormErrors: {
+          server: [],
+          form: action.errors,
+        },
+      });
   default:
     return state;
   }
