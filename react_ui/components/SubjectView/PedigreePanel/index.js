dd3f4c532b82189a0925078bcf82d3a42a2ca93c
seg1129 <geraces@email.chop.edu>
2019-02-13 15:29:10 -0500

    reloacating dispatch calls to get relationships and all subjects in protocol to pedigree panel index for better handling of the data in the child react components

--- react_ui/components/SubjectView/PedigreePanel/index.js
+++ react_ui/components/SubjectView/PedigreePanel/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import * as PedigreeActions from '../../../actions/pedigree';
+import * as SubjectActions from '../../../actions/subject';
 import PedigreeCardView from './PedigreeCardView';
 import { connect } from 'react-redux';
 
@@ -11,10 +13,49 @@ import { connect } from 'react-redux';
 //   path: React.PropTypes.string,
 // };
 class PedigreePanel extends React.Component {
-  render() {
-    return (<PedigreeCardView />);
+  componentDidMount() {
+    const { dispatch } = this.props;
+    dispatch(PedigreeActions.fetchPedigree(
+      this.props.activeProtocolId,
+      this.props.activeSubject.id));
+    dispatch(SubjectActions.fetchSubjects(this.props.activeProtocolId));
   }
+
+  componentWillUnmount() {
+    const { dispatch } = this.props;
+    dispatch(PedigreeActions.clearPedigreeState());
+  }
+
+
+    render() {
+      return (<PedigreeCardView />);
+    }
+}
+PedigreePanel.propTypes = {
+  dispatch: React.PropTypes.func,
+  protocol: React.PropTypes.object,
+  activeSubject: React.PropTypes.object,
+  activeRecord: React.PropTypes.object,
+  activeProtocolId: React.PropTypes.number,
+  pedigree: React.PropTypes.object,
+  isFetching: React.PropTypes.bool,
+  subjects: React.PropTypes.array,
+};
+
+function mapStateToProps(state) {
+  return {
+    pedigree: {
+      isFetching: state.pedigree.isFetching,
+      items: state.pedigree,
+    },
+    activeSubject: state.subject.activeSubject,
+    activeRecord: state.record.activeRecord,
+    activeProtocol: state.protocol.activeProtocol,
+    activeProtocolId: state.protocol.activeProtocolId,
+    subjects: state.subject.items,
+  };
 }
 
-export default PedigreePanel;
+export default connect(mapStateToProps)(PedigreePanel);
+// export default PedigreePanel;
 // export default connect(SubjectPedigreePanel);
