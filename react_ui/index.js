0168f02dac5f65ae5e5d8caf22423033504f1013
seg1129 <geraces@email.chop.edu>
2019-02-15 16:37:36 -0500

    adjusting routing of BRP homepage

--- react_ui/index.js
+++ react_ui/index.js
@@ -6,7 +6,8 @@ import ProjectMenu from './components/ProjectMenu';
 import SubjectSelect from './components/SubjectSelect';
 import SubjectView from './components/SubjectView';
 import { Provider } from 'react-redux';
-import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import { Router, Route, BrowserRouter} from 'react-router-dom';
+import { createBrowserHistory } from 'history';
 import { render } from 'react-dom';
 import store from './store';
 
@@ -17,28 +18,39 @@ class ProjectList extends React.Component {
 // Very manually constraining this to the root path until we establish proper
 // routing and views. This allows us to bounce out of the single page app paradigm
 // and into existing ehb-datasource url paths
-  if ( window.location.pathname === '/' ) {
-    render(
-      <Provider store={store}>
-        <Router history={browserHistory}>
-          <Route path="/" component={App}>
-            <IndexRoute component={ProjectMenu} />
-            <Route path="dataentry/protocol/:id" component={SubjectSelect} />
-            <Route
-              path="dataentry/protocol/:prot_id/subject/:sub_id(/:edit)"
-              component={SubjectView}
-            />
-          </Route>
-        </Router>
-      </Provider>,
-      document.getElementById('react')
-    );
-  } else {
-    render(
+
+  constructor() {
+    const history = createBrowserHistory();
+    console.log("ummm ok ")
+  }
+
+
+    render(){
+      if ( history.location.pathname === '/' ) {
+        console.log("ummm ok ")
+        return (
+          <Provider store={store}>
+            <Router history={browserHistory}>
+
+                <Route path="/" exact component={ProjectMenu} />
+                <Route path="dataentry/protocol/:id" component={SubjectSelect} />
+                <Route
+                  path="dataentry/protocol/:prot_id/subject/:sub_id(/:edit)"
+                  component={SubjectView}
+                />
+
+            </Router>
+          </Provider>,
+          document.getElementById('react')
+        );
+    } else {
+      console.log("ummm ok 2")
+    return(
       <Provider store={store}>
         <Navbar />
       </Provider>,
       document.getElementById('react'));
+    }
   }
 }
 export default ProjectList;
