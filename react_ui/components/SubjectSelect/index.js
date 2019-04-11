72d82a92327eaa76ade582bd85493544581a641c
seg1129 <geraces@email.chop.edu>
2019-04-11 16:09:20 -0400

    continued work on getting subject Griddle list to work on react v16

--- react_ui/components/SubjectSelect/index.js
+++ react_ui/components/SubjectSelect/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Griddle from 'griddle-react';
+import Griddle, { pageProperties, plugins, RowDefinition, ColumnDefinition}from 'griddle-react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import RaisedButton from '@material-ui/core/Button';
@@ -15,13 +15,16 @@ class SubjectSelect extends React.Component {
     super(props);
     this.handleNewSubject = this.handleNewSubject.bind(this);
     this.handleClick = this.handleClick.bind(this);
+    const { dispatch } = this.props;
   }
 
   componentDidMount() {
     const { dispatch } = this.props;
     this.props.dispatch(ProtocolActions.setActiveProtocol(this.props.match.params.id));
     // Check to see if subjects are loaded, if not fetch them
-    this.props.dispatch(SubjectActions.fetchSubjects(this.props.match.params.id));
+    if(this.props.subject.items.length == 0){
+      this.props.dispatch(SubjectActions.fetchSubjects(this.props.match.params.id));
+    }
   }
 
   getActiveProtocol() {
@@ -34,6 +37,33 @@ class SubjectSelect extends React.Component {
       }, this);
     }
     return activeProtocol;
+  }
+
+  griddleFrendlySubjects() {
+    let orgIdLabel = 'Identifier';
+    let subs = [];
+    let row = {};
+    let rows = [];
+    if (this.props.subject.items.length >0) {
+      subs = this.props.subject.items.map((sub) => {
+        let externalIds = '';
+        sub.external_ids.forEach((exId) => {
+          externalIds += `${exId.label_desc} : ${exId.record_id}\n`;
+        }, this);
+        let row = {
+          Organization: sub.organization_name,
+          'First Name': sub.first_name,
+          'Last Name': sub.last_name,
+          'Birth Date': sub.dob,
+          'External IDs': externalIds,
+          subject: sub,
+        };
+        row[orgIdLabel] = sub.organization_subject_id;
+        // return row
+        rows.push(row);
+      });
+    }
+    return rows
   }
 
   handleNewSubject() {
@@ -59,6 +89,7 @@ class SubjectSelect extends React.Component {
     // If this view is navigated to directly. Get active protocol based on param
     const subjects = this.props.subject.items;
     const protocol = this.getActiveProtocol();
+    const rows = this.griddleFrendlySubjects();
 
     let manageExternalIDs = false;
     const subjectCountStyle = {
@@ -85,28 +116,9 @@ class SubjectSelect extends React.Component {
     if (manageExternalIDs) {
       columns.push('External IDs');
     }
-    // Transform subjects into Griddle friendly format
-    let subs = [];
-    if (subjects) {
-      subs = subjects.map((sub) => {
-        let externalIds = '';
-        sub.external_ids.forEach((exId) => {
-          externalIds += `${exId.label_desc} : ${exId.record_id}\n`;
-        }, this);
-        let row = {
-          Organization: sub.organization_name,
-          'First Name': sub.first_name,
-          'Last Name': sub.last_name,
-          'Birth Date': sub.dob,
-          'External IDs': externalIds,
-          subject: sub,
-        };
-        row[orgIdLabel] = sub.organization_subject_id;
-        return row
-      });
-    }
+
     return (
-      protocol ?
+      this.props.protocol ?
         <div>
           {this.props.subject.addSubjectMode ?
             <NewSubjectForm orgs={this.props.protocol.orgs} /> :
@@ -116,7 +128,7 @@ class SubjectSelect extends React.Component {
             <BackButton /> :
             <div />
           }
-          <h3>Project: {protocol.name}</h3>
+          <h3>Project: {this.props.protocol.name}</h3>
           <div id="toolbar">
             <ul className="list-unstyled">
               <li>
@@ -129,18 +141,27 @@ class SubjectSelect extends React.Component {
               </li>
             </ul>
           </div>
-          <div style={subjectCountStyle}>{subs.length} Subjects</div>
+          <div style={subjectCountStyle}>{this.props.subject.items.length} Subjects</div>
           <div className="subject-table">
-            {!this.props.subject.isFetching ?
+            {(this.props.subject.items.length>0) ?
               <Griddle
-                showFilter
                 onRowClick={this.handleClick}
-                resultsPerPage={10}
-                results={subs}
-                columns={columns}
-                tableClassName={'subject-table'}
-                useGriddleStyles={false}
-              /> : <LoadingGif />}
+                data={rows}
+                >
+                pageProperties={{
+                  currentPage: 1,
+                  pageSize: 10,
+                  recordCount: 10000,
+                }}
+                <RowDefinition>
+                  <ColumnDefinition id="organization_name" title="Organization" />
+                  <ColumnDefinition id="organization_subject_id" title="Identifier" />
+                  <ColumnDefinition id="first_name" title="First Name" />
+                  <ColumnDefinition id="last_name" title="Last Name" />
+                  <ColumnDefinition id="dob" title="Birth Date" />
+                </RowDefinition>
+                </Griddle>
+               : <LoadingGif />}
           </div>
         </div> :
         <div />
@@ -162,6 +183,7 @@ function mapStateToProps(state) {
       items: state.protocol.items,
       activeProtocolId: state.protocol.activeProtocolId,
       orgs: state.protocol.orgs,
+      pds: state.pds,
     },
     subject: {
       items: state.subject.items,
