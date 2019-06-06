1d4c6c82c21e3825ab3c759abf56c4459efdc882
seg1129 <geraces@email.chop.edu>
2019-06-06 17:42:08 -0400

    updating handling of selecting org in org select field #232

--- react_ui/components/SubjectView/SubjectPanel/SubjectOrgSelectField.js
+++ react_ui/components/SubjectView/SubjectPanel/SubjectOrgSelectField.js
@@ -24,7 +24,7 @@ class SubjectOrgSelectField extends React.Component {
     return orgList
   }
   onChange(e, index, value) {
-    console.log(e.target.value)
+    console.log(e.value)
     const { dispatch } = this.props;
     // Check to see if we're editing an existing subject
     if (!this.props.new) {
