09900a9dd4c21cc1bb1fda8671bab74a5b6af426
SueG <geraces@email.chop.edu>
2019-06-12 15:09:44 -0400

    Sorting subjects by Modified date (#244)
    * sorting subjects by modified date
    
    * adding logging for when getting subjects from cache to see if all subjects have modified date or not, and sorting by modified date when pulling from cache if all subjects have modified dates
    
    * fixing error
    
    * updating logging for sorting subjects with PK with protocol number to help me troubleshoot this issue later

--- api/views/protocol.py
+++ api/views/protocol.py
@@ -1,7 +1,7 @@
 import logging
 import json
-
 from datetime import datetime
+
 from copy import deepcopy
 
 from django.core.cache import cache
@@ -129,8 +129,15 @@ class ProtocolSubjectsView(BRPApiView):
         # Check cache
         cache_data = cache.get('protocol{0}_sub_data'.format(p.id))
         if cache_data:
+            # if all subjects have a modified date in cache then sort using modified date
+            try:
+                subs = sorted(json.loads(cache_data), key=lambda i: datetime.strptime(i['modified'], '%Y-%m-%dT%H:%M:%S.%f'), reverse=True)
+            # if some subjects do not have modified date then sort by PK
+            except:
+                logger.info('subjects sorted by primary key for protocol {protocol}'.format(protocol=p.id))
+                subs = sorted(json.loads(cache_data), key=lambda i: (i['id'], '%Y-%m-%dT%H:%M:%S.%f'), reverse=True)
             return Response(
-                json.loads(cache_data),
+                subs,
                 headers={'Access-Control-Allow-Origin': '*'}
             )
         if p.isUserAuthorized(request.user):
@@ -189,6 +196,7 @@ class ProtocolSubjectsView(BRPApiView):
             )
 
         if subjects:
+            subs = sorted(subs, key=lambda kv: datetime.strptime(kv['modified'], '%Y-%m-%dT%H:%M:%S.%f'), reverse=True)
             return Response(
                 subs,
                 headers={'Access-Control-Allow-Origin': '*'}
