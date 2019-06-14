2e601b38508329d082c3f2cbbf4c8ee04678f32c
gonzalezak <gonzalezak@email.chop.edu>
2019-06-14 08:40:36 -0400

    Added Backbutton CSS and fixed LoadingGif component

--- react_ui/components/BackButton/index.js
+++ react_ui/components/BackButton/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { withRouter } from 'react-router';
+import styles from './BackButton.css';
 
 class BackButton extends React.Component {
   constructor(props) {
@@ -14,30 +15,10 @@ class BackButton extends React.Component {
   }
 
   render() {
-    const divStyle = {
-      backgroundColor: 'white',
-      opacity: '0.7',
-      width: '75px',
-      position: 'fixed',
-      zIndex: 99,
-      left: '90px',
-      bottom: '10px',
-      height: '75px',
-      textAlign: 'center',
-      borderRadius: '50%',
-      boxShadow: '5px 5px 5px rgba(204, 197, 185, 0.7)',
-      cursor: 'pointer',
-    };
-    const arrowStyle = {
-      marginTop: '10px',
-      fontSize: '2em',
-      color: '#7AC29A',
-      position: 'relative',
-      top: '20px',
-    };
+  
     return (
-      <div onClick={this.handleClick} style={divStyle}>
-        <i style={arrowStyle} className="ti-arrow-left"></i>
+      <div onClick={this.handleClick} className={styles.divStyle}>
+        <i className={styles.tiArrowLeft}></i>
       </div>
     );
   }
