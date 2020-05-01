71914b0e960ee719af05e0ca51f7e6d1505ee8ef
Pagan <paganm@0068ebe45112.chop.edu>
2020-05-01 15:46:18 -0400

    ProtocolUserCredential Filtering Works

--- brp_admin/views/test.py
+++ brp_admin/views/test.py
@@ -8,6 +8,7 @@ from django.core.exceptions import ObjectDoesNotExist
 from brp_admin.forms import ProtocolUserForm, ProtocolUserCredentialsForm, ProtocolForm
 from api.models.protocols import ProtocolUser, ProtocolUserCredentials, Protocol, ProtocolDataSource
 from django.views.generic.base import TemplateResponseMixin
+from django.shortcuts import get_object_or_404
 
 class UpdateCredsView(TemplateView):
 
@@ -16,47 +17,36 @@ class UpdateCredsView(TemplateView):
     def __init__(self):
         self.protocol_user_credentials = ProtocolUserCredentialsForm()
 
-    def processCredsForm(self, request):
-
-        context = {}
-        creds_form = ProtocolUserCredentialsForm(data=request.POST)
-
-        # If the form is valid and the method is POST
-        if creds_form.is_valid() and request.method == 'POST':
-            # Clean the data
-            data = creds_form.cleaned_data
-
-            # Filter based on the credentials the user gave in the input
-            credentials = ProtocolUserCredentials.objects.filter(protocol=data['protocol'],user=data['user'],
-            protocol_user=data['protocol_user'],
-            data_source=data['data_source'],data_source_username=data['data_source_username'])
-
-            data_source_password = data['data_source_password']
-
-            # Iterate through the queryset, set the old password to the new password user gave
-            # for each password in every object with the criteria above
-            for cred in credentials:
-                cred.data_source_password = data_source_password
-
-        if not creds_form.is_valid():
-            if not (creds_form.non_field_errors()):
-                context['form_errors'] = creds_form.errors
-                context['form1'] = creds_form
-
-
     def get(self, request):
         context = {}
         allcreds = ProtocolUserCredentials.objects.all()
-        context = {'form1': ProtocolUserCredentialsForm()}
+        context = {'form1': ProtocolUserCredentialsForm(), 'allcreds': allcreds}
 
         return render(request, 'update_user_creds.html', context)
 
     def post(self, request):
         post_data = request.POST
+        creds_form = ProtocolUserCredentialsForm(data=request.POST)
         context = {}
+        protocol = post_data['protocol']
+        user=post_data['user']
+        protocol_user=post_data['protocol_user']
+        data_source=post_data['data_source']
+        data_source_username=post_data['data_source_username']
+        data_source_password=post_data['data_source_password']
 
         # If user clicks submit button, go to the confirmation page
         if 'submit_nautilus_creds' in post_data:
-            form = self.processCredsForm(request)
-            context = {'form': form}
-            return render(request, 'confirm_nautilus_creds.html', context)
+            # If the form is valid and the method is POST
+            if creds_form.is_valid:
+            # Filter ProtocolUserCredentials objects by the credentials user entered
+                credentials = ProtocolUserCredentials.objects.filter(protocol=protocol,user=user,protocol_user=protocol_user,
+                data_source=data_source,data_source_username=data_source_username,data_source_password=data_source_password)
+
+                for cred in credentials:
+                    creds.data_source_password = data_source_password
+
+            context = {'protocol':protocol, 'user': user, 'protocol_user':protocol_user,
+            'data_source':data_source,'data_source_username':data_source_username, 'data_source_password': data_source_password}
+
+            return render(self.request, 'confirm_nautilus_creds.html', context)
